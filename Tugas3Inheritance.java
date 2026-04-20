517874a24f73dfcdd0b2e3b3d35c459a21d6e413
𝚆𝚘𝚠. 𝙲𝚘𝚖 <160624200+Muhamd-ilham@users.noreply.github.com>
2026-04-20 14:32:38 +0700

    Update Tugas3Inheritance.java

--- Tugas3Inheritance.java
+++ Tugas3Inheritance.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Locale;
 
 class Mahasiswa {
     String nim, nama;
@@ -14,7 +15,6 @@ class Mahasiswa {
 }
 
 class DataNilai extends Mahasiswa {
-    
     public DataNilai(String nim, String nama, double nilai) {
         super(nim, nama, nilai);
         hitungGrade();
@@ -38,26 +38,37 @@ class DataNilai extends Mahasiswa {
             status = "tidak lulus";
         } else {
             grade = "Salah";
-            System.out.println("Input nilai anda salah"); [cite: 378]
         }
     }
 }
 
 public class Tugas3Inheritance {
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in); [cite: 372]
+        Scanner input = new Scanner(System.in).useLocale(Locale.US);
         ArrayList<DataNilai> listMhs = new ArrayList<>();
         
         System.out.print("Masukkan Jumlah Mahasiswa: ");
         int jumlah = input.nextInt();
+        input.nextLine();
+
         double totalNilai = 0;
 
         for (int i = 0; i < jumlah; i++) {
-            System.out.println("\nMahasiswa ke-" + (i + 1));
-            System.out.print("NIM   : "); String nim = input.next();
-            System.out.print("Nama  : "); String nama = input.next();
-            System.out.print("Nilai : "); double nilai = input.nextDouble();
+            System.out.println("\nInput Data Mahasiswa ke-" + (i + 1));
+            System.out.print("NIM   : "); 
+            String nim = input.nextLine(); 
             
+            System.out.print("Nama  : "); 
+            String nama = input.nextLine(); 
+            
+            System.out.print("Nilai : "); 
+            double nilai = input.nextDouble();
+            input.nextLine(); 
+
+            if (nilai < 0 || nilai > 100) {
+                System.out.println("Input nilai anda salah!");
+            }
+
             DataNilai mhs = new DataNilai(nim, nama, nilai);
             listMhs.add(mhs);
             totalNilai += mhs.nilai;
@@ -65,10 +76,6 @@ public class Tugas3Inheritance {
 
         StringBuilder lulus = new StringBuilder();
         StringBuilder tdkLulus = new StringBuilder();
-        StringBuilder gradeA = new StringBuilder();
-        StringBuilder gradeB = new StringBuilder();
-        StringBuilder gradeD = new StringBuilder();
-        
         int nLulus = 0, nTdkLulus = 0, nA = 0, nB = 0, nD = 0;
 
         System.out.println("\n--- HASIL OUTPUT ---");
@@ -82,18 +89,17 @@ public class Tugas3Inheritance {
                 nTdkLulus++;
                 tdkLulus.append(m.nama).append(", ");
             }
-
-            if (m.grade.equals("A")) { nA++; gradeA.append(m.nama).append(", "); }
-            if (m.grade.equals("B")) { nB++; gradeB.append(m.nama).append(", "); }
-            if (m.grade.equals("D")) { nD++; gradeD.append(m.nama).append(", "); }
+            if (m.grade.equals("A")) nA++;
+            if (m.grade.equals("B")) nB++;
+            if (m.grade.equals("D")) nD++;
         }
 
         System.out.println("Jumlah Mahasiswa: " + jumlah);
         System.out.println("Jumlah Mahasiswa yg Lulus: " + nLulus + " yaitu " + lulus);
         System.out.println("Jumlah Mahasiswa yg Tidak Lulus: " + nTdkLulus + " yaitu " + tdkLulus);
-        System.out.println("Jumlah Mahasiswa dengan Nilai A = " + nA + " yaitu " + gradeA);
-        System.out.println("Jumlah Mahasiswa dengan Nilai B = " + nB + " yaitu " + gradeB);
-        System.out.println("Jumlah Mahasiswa dengan Nilai D = " + nD + " yaitu " + gradeD);
-        System.out.println("Rata-rata nilai mahasiswa adalah: " + (totalNilai / jumlah));
+        System.out.println("Jumlah Mahasiswa dengan Nilai A = " + nA);
+        System.out.println("Rata-rata nilai: " + (totalNilai / jumlah));
+        
+        input.close();
     }
 }
